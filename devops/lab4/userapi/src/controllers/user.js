f2be9d84117a512225ea3a8c5ad4f6a49f19add4
theo_rdk <theophile.tarbe-de-saint-hardouin@edu.ece.fr>
2020-10-21 11:19:21 +0200

    lab4 for quidux

---
+++ devops/lab4/userapi/src/controllers/user.js
@@ -0,0 +1,27 @@
+const client = require('../dbClient')
+
+module.exports = {
+  create: (user, callback) => {
+    // Check parameters
+    if(!user.username)
+      return callback(new Error("Wrong user parameters"), null)
+    // Create User schema
+    const userObj = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    }
+    // Save to DB
+    // TODO check if user already exists
+    client.hmset(user.username, userObj, (err, res) => {
+      if (err) return callback(err, null)
+      callback(null, res) // Return callback
+    })
+  },
+  get: (username, callback) => {
+    // TODO create this method : Successful get user
+    client.hmget(username, "firstname", "lastname", (err, res) => {
+      if (err) return callback(err,null)
+      callback(null,res) // Return callback
+    })
+  }
+}
