53034c43663af9c43dbfbb106d124fea23fc6a31
theo_rdk <theophile.tarbe-de-saint-hardouin@edu.ece.fr>
2021-03-15 18:20:47 +0100

    add ping pong

---
+++ SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.pingpong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(String.valueOf(v.getTag()).equals("newgame")){
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }else{
+                //nothing for the moment
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_newGame = (Button)findViewById(R.id.btn_newGame);
+        btn_newGame.setOnClickListener(onClickListener);
+
+        Button btn_history = (Button)findViewById(R.id.btn_history);
+        btn_history.setOnClickListener(onClickListener);
+    }
+}
