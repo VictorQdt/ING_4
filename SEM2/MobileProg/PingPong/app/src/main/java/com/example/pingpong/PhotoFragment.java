5ce3f0e6fd3a3030d2eecb64566ee13aa59e712c
theo_rdk <theophile.tarbe-de-saint-hardouin@edu.ece.fr>
2021-03-15 20:54:15 +0100

    take & save picture ok

--- SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/PhotoFragment.java
+++ SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/PhotoFragment.java
@@ -1,19 +1,130 @@
 package com.example.pingpong;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class PhotoFragment extends Fragment {
+
+    //constante
+    private static final int RETOUR_PRENDRE_PHOTO = 1;
+
+    //propriétés
+    private Button btnTakePhoto;
+    private ImageView imgDisplayPhoto;
+    private Button btnSave;
+    private String photoPath = null;
+    private Bitmap img;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+        //initActivity();
+        View picture_view = inflater.inflate(R.layout.fragment_photo, container, false);
+
+        //récupération des objets graphiques
+        btnTakePhoto = (Button) picture_view.findViewById(R.id.btnTakePicture);
+        imgDisplayPhoto = (ImageView) picture_view.findViewById(R.id.imgDisplayPhoto);
+        btnSave = (Button) picture_view.findViewById(R.id.btnSave);
+
+        //méthode pour gérer les évènements
+        createOnClickBtnTakePhoto();
+        createOnClickBtnSave();
+
+        return picture_view;
+    }
+
+    /*
+     * évènement clic sur boutton btnTakePhoto
+     * */
+    private void createOnClickBtnTakePhoto(){
+        btnTakePhoto.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePhoto();
+            }
+        });
+    }
+
+    /**
+     * évènement click sur Save
+     */
+    private void createOnClickBtnSave() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // enrengistrer la photo
+                MediaStore.Images.Media.insertImage(getActivity().getContentResolver(), img, "mon img", "img");
+            }
+        });
+    }
+
+    /**
+     * Accès à l'appareil photo et mémorise dans un fichier temporaire (avec le provider)
+     */
+    private void takePhoto(){
+        //creer un intent pour ouvrir une fenetre pour prendre une photo
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //test pour controler que l'intent peut etre géré par le telephone
+        if(intent.resolveActivity(getActivity().getPackageManager()) != null){
+            //creer un nom de fichier unique qui récupère sous forme de string la date actuelle
+            String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            //chemin du directory où sont stockés les photos
+            File photoDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            try {
+                File photoFile = File.createTempFile("photo"+time, ".jpg", photoDir);
+                //Enrengistrer le chemin complet
+                photoPath = photoFile.getAbsolutePath();
+                // créer l'URI pour accéder au fichier (Context, provider, fichier)
+                Uri photoUri = FileProvider.getUriForFile(
+                        getActivity(),
+                        PhotoFragment.this.getActivity().getApplicationContext().getPackageName() + ".provider",
+                        photoFile);
+                // transfert uri vers l'intent pour enrengistrement photo dans fichier temporaire
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                //ouvrir l'activity par rapport à l'intent
+                startActivityForResult(intent, RETOUR_PRENDRE_PHOTO);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * retour de l'appel de l'appareil photo (startActivityforResult)
+     * @param requestCode = retour prendrePhoto (qui a appelé)
+     * @param resultCode = est-ce que il y a bien eu un retour
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // vérifier le bon code de retour (requestCode) et l'état du retour (resultCode)
+        if(requestCode == RETOUR_PRENDRE_PHOTO && resultCode == Activity.RESULT_OK){
+            //récupérer l'image
+            img = BitmapFactory.decodeFile(photoPath);
+            //afficher l'image
+            imgDisplayPhoto.setImageBitmap(img);
+        }
     }
 }
