5f9340712af9370bfedb619dd983c14018e2712f
theo_rdk <theophile.tarbe-de-saint-hardouin@edu.ece.fr>
2021-03-15 19:30:14 +0100

    add bottom nav

--- SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/GameActivity.java
+++ SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/GameActivity.java
@@ -1,8 +1,17 @@
 package com.example.pingpong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static com.example.pingpong.R.id.nav_home;
+import static com.example.pingpong.R.id.nav_map;
+import static com.example.pingpong.R.id.nav_photo;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +19,39 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //We open the HomeFragment first by default
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    //Bottom Navigation Listener
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    //Create the fragments
+                    switch (item.getItemId()) {
+                        case nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case nav_photo:
+                            selectedFragment = new PhotoFragment();
+                            break;
+                    }
+
+                    //Show the fragment selected
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    //Return a true boolean because we selected an item (the fragment)
+                    return true;
+                }
+            };
 }
--- SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/MainActivity.java
+++ SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.pingpong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
