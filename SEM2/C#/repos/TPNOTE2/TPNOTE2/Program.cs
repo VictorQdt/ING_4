5a3d10c364f5f83fb813405f71860a17c4ca428b
theordk <theophile.tarbe@hotmail.fr>
2021-03-22 16:28:27 +0100

    base of the app

--- SEM2/C#/repos/TPNOTE2/TPNOTE2/Program.cs
+++ SEM2/C#/repos/TPNOTE2/TPNOTE2/Program.cs
@@ -16,7 +16,7 @@ namespace TPNOTE2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
