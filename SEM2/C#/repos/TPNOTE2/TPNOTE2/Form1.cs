4f076c7a2fcc0445779c71721feb222981b051e3
theordk <theophile.tarbe@hotmail.fr>
2021-03-22 15:20:48 +0100

    add tp note 2

---
+++ SEM2/C#/repos/TPNOTE2/TPNOTE2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TPNOTE2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DatabaseTP2.mdf;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initialiser();
+
+        }
+
+        private void initialiser()   //procédure qui permet de charger le contenu de la table client dans le Datagridview
+        {
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("select * from [table]", connection);
+            SqlDataReader dataReader = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataTable;
+            dataReader.Close();
+            connection.Close();
+        }
+    }
+}
