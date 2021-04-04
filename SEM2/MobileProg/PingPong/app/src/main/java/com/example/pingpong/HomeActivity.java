49e93d149fdeaa0043818fc63a428e9e626f8dd8
theo_rdk <theophile.tarbe-de-saint-hardouin@edu.ece.fr>
2021-04-04 19:45:43 +0200

    map over & language

--- SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/HomeActivity.java
+++ SEM2/MobileProg/PingPong/app/src/main/java/com/example/pingpong/HomeActivity.java
@@ -1,15 +1,21 @@
 package com.example.pingpong;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static com.example.pingpong.R.id.nav_home;
@@ -23,6 +29,9 @@ import static com.example.pingpong.R.id.nav_photo;
  */
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     /**
      * Listener of the bottom navigation bar
      * When choosing an option, a new Fragment is created
@@ -33,6 +42,7 @@ public class HomeActivity extends AppCompatActivity {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
+                    Activity selectedActivity = null;
                     //Create the fragments
                     switch (item.getItemId()) {
                         case nav_home:
@@ -47,9 +57,13 @@ public class HomeActivity extends AppCompatActivity {
                             }
                             break;
                         case nav_map:
-                            selectedFragment = new MapFragment();
-                            //Show the fragment selected
-                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                            if(isServicesOK()){
+                                selectedFragment = new MapFragment();
+                                //Show the fragment selected
+                                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                            } else {
+                                Log.d(TAG, "nav_map : access denied");
+                            }
                             break;
                         case nav_photo:
                             selectedFragment = new PhotoFragment();
@@ -75,5 +89,27 @@ public class HomeActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NewMatchFragment()).commit();
 
+    }
+
+    /**
+     * Check services if option Map is choosen by the user
+     * @return
+     */
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK : checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(HomeActivity.this);
+        if (available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map request
+            Log.d(TAG,"isServicesOK: Google Play services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            // an error occured but we can resolve it
+            Log.d(TAG,"isServicesOK: an error occured but we can resolve it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(HomeActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Map request not available", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
